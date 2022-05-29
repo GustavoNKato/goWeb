425eb369fa03014f708fd38c954e737682792f1f
GustavoNKato <gustavo.nkato@gmail.com>
2022-05-29 20:26:07 -0300

    feature: adding endpoint to list all products

--- main.go
+++ main.go
@@ -1,13 +1,44 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	router := gin.Default()
+	router.GET("/products", productHandler)
+	router.Run()
+
+}
+
+type product struct {
+	Id          int     `json:"id"`
+	Name        string  `json:"name"`
+	Color       string  `json:"color"`
+	Price       float64 `json:"price"`
+	Inventory   int     `json:"inventory"`
+	Code        string  `json:"code"`
+	Publication bool    `json:"publication"`
+	Data        string  `json:"data"`
+}
+
+func productHandler(ctx *gin.Context) {
+
+	var products []product
+
+	jsonFile, err := os.ReadFile("products.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal(jsonFile, &products); err != nil {
+		log.Fatal(err)
+	}
+
+	ctx.IndentedJSON(http.StatusOK, products)
 }
