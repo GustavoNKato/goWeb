6d98fbf3f3e3dd0b8e776bbfe853afcde0bde0e2
GustavoNKato <gustavo.nkato@gmail.com>
2022-05-30 16:39:05 -0300

    feat: add get by id of product

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func main() {
 
 	router := gin.Default()
 	router.GET("/products", productHandler)
+	router.GET("/products/:id", getProductByIdHandler)
 	router.Run()
 
 }
@@ -28,18 +30,38 @@ type product struct {
 }
 
 func productHandler(ctx *gin.Context) {
-
 	var products []product
+	products = readJsonFile(products)
+	ctx.IndentedJSON(http.StatusOK, products)
+}
 
+func getProductByIdHandler(ctx *gin.Context) {
+	var products []product
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		log.Println("precisa ser um int - ", err)
+	}
+
+	products = readJsonFile(products)
+
+	for _, product := range products {
+		if product.Id == id {
+			ctx.IndentedJSON(http.StatusOK, product)
+			return
+		}
+	}
+	ctx.IndentedJSON(http.StatusNotFound, gin.H{"message": "produto não encontrado"})
+
+}
+
+func readJsonFile(products []product) []product {
 	jsonFile, err := os.ReadFile("products.json")
 	if err != nil {
-		log.Println("Erro ao ler o arquivo -", err)
-
+		log.Println("erro ao ler o arquivo -", err)
 	}
 
 	if err := json.Unmarshal(jsonFile, &products); err != nil {
-		log.Println("Erro no momento do decode no arquivo json -", err)
+		log.Println("erro no momento do decode no arquivo json -", err)
 	}
-
-	ctx.IndentedJSON(http.StatusOK, products)
+	return products
 }
